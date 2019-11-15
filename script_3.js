f0c390cac8328ca62029a2abf8b7158d7f5882c2
ludo <ludovict40@gmail.com>
2019-11-15 13:15:19 +0100

    modif exo 3 et ajout exo 6

--- script_3.js
+++ script_3.js
@@ -4,7 +4,7 @@ console.log(pyramid(chiffre))
 
 function pyramid(n) {
     var espace = n
-    for (let colonne = 0; colonne < n; colonne++) {
+    for (let colonne = 1; colonne <= n; colonne++) {
         console.log(" ".repeat(espace) + "#".repeat(colonne))
         espace--;
     }
--- script_6.js
+++ script_6.js
@@ -1,27 +1,53 @@
 function main() {
     let adn = prompt("Entrez l'adn")
-    for (let i = 0; i < adn.length; i++) {
-        retourne_condon(adn)
-    }
-    
+    let condons = retourne_condon(adn,3)
+    console.log(condons)
+    proteines(condons)
 }
 
 main()
 
 
-function retourne_condon(adn){
+function retourne_condon(adn, nombre){
     adn = adn.split("")
-
+    let condon = []
+    let inter = ""
+    let compte = nombre
     for (let i = 0; i < adn.length; i++){
-        if (i < 3) {
-            let condon = []
-            condon.push(adn[i])
-            if (condon.length === 3) {
-                console.log(condon)
-            }
+        inter += adn[i]
+        compte--
+        if (inter.length === 3) {
+            condon.push(inter)
+            inter = ""
+            compte = nombre
         }
-        if(i > 3){
-            adn.splice(adn[i],1)
-        }
+    }
+    return condon
+}
+
+function proteines(condons) {
+    let condon = ""
+    let proteines = []
+    for(i in condons){
+        condon += condons[i]
+        proteines.push(verif_condons(condon))
+        condon = ""
+    }
+    console.log(proteines)
+}
+
+function verif_condons(condon) {
+    if (condon.includes("UC") || condon.includes("AG")){
+        return "Sérine"
+    }else if (condon.includes("CC")) {
+        return "Proline"
+    }else if (condon.includes("UUA") || condon.includes("UUG")) {
+        return "Leucine"
+    }else if (condon.includes("UUU") || condon.includes("UUC")){
+        return "Phénylalanine"
+    }else if (condon.includes("GG") || condon.includes("GA") || condon.includes("CGU") || condon.includes("CGC")) {
+        return "Arginine"
+    }else if (condon.includes("UAU") || condon.includes("UAC")) {
+        return "Tyrosine"
     }
 }
